dc0d1f6a3ed509eb319286e4ec1373587e8e15af
Abdirezak Mundino Datago <seadbis3@gmail.com>
2022-07-05 15:46:02 -0700

    modified 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,11 @@
-#include<stdio.h>
-
 /**
  * main - Entry point
  *
  * return: Always return 0(Success)
  */
-int main(void)
+void print_alphabet(void)
 {
-	for(int i=97; i<123;i++)
+	for (int i = 97; i < 123; i++)
 	{
 		putchar((char)i);
 		putchar('\n');
