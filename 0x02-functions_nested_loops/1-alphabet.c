3a5dd018e447a4f1bbb828787eb891eaf5970269
Abdirezak Mundino Datago <seadbis3@gmail.com>
2022-07-05 15:51:38 -0700

    modified 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,6 +3,12 @@
  *
  * return: Always return 0(Success)
  */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
 void print_alphabet(void)
 {
 	for (int i = 97; i < 123; i++)
@@ -10,5 +16,4 @@ void print_alphabet(void)
 		putchar((char)i);
 		putchar('\n');
 	}
-	return (0);
 }
