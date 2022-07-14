90b5fbb4428dfe9452a60fcb8e992edda361438d
Abdirezak Mundino Datago <seadbis3@gmail.com>
2022-07-14 10:39:33 -0700

    added

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates @src and @dest
+ *
+ * @src: the source string
+ * @dest: the destination string
+ *
+ * Return: a ponter to the concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0;
+	int dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
