18ad02d51ff749d7b51cbe611c2782c47ada48c0
Abdirezak Mundino Datago <seadbis3@gmail.com>
2022-07-14 15:28:40 -0700

    modified one

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 char *_strcat(char *dest, char *src);
-char *_srtncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strncmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
